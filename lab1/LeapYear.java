75efaaec483198dcded1011df38d6b3bf078fac0
Gong Qiong <gongqiongbnu@gmail.com>
2019-07-29 18:51:41 +0900

    finish leapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,21 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Gong Qiong
  */
 public class LeapYear {
+	/** Method that determines whether or not a year is a leap year.
+	* return a boolean value.
+	*/
+	public static boolean isLeapYear(int year){
+		if (year%400 == 0){
+			return true
+		}
+		else if (year%4 ==0 & year%100 !=0){
+			return true
+		}
+		else{
+			return false
+		}
+	}
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
