1e7de2ac8cca6e06eccb09e8a5ae00983bfc35c3
gong <gongqiongbnu@gmail.com>
2020-05-28 21:17:04 +0800

    maxQueue

--- hw6/Boggle.java
+++ hw6/Boggle.java
@@ -1,6 +1,9 @@
-import java.util.*;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
 
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MaxPQ;
 
 public class Boggle {
     
@@ -18,6 +21,17 @@ public class Boggle {
         return trie;
     }
     
+    private static class LenComparator implements Comparator<String> {
+        
+        @Override
+        public int compare(String o1, String o2) {
+            if (o1 == o2) {
+                return o2.compareTo(o1);
+            }
+            return o1.length() - o2.length();
+        }
+    }
+    
     /**
      * Solves a Boggle puzzle.
      *
@@ -32,88 +46,42 @@ public class Boggle {
         // YOUR CODE HERE
         Board b = new Board(boardFilePath);
         trie = getTrie();
-        Set<String> res = new HashSet<>();
+        MaxPQ<String> words = new MaxPQ<>(new LenComparator());
+        
         for (int i = 0; i < b.row; i += 1) {
             for (int j = 0; j < b.col; j += 1) {
                 List<Board.Node> marked = new ArrayList<>();
                 Board.Node n = b.new Node(i, j);
                 String prefix = n.ch.toString();
                 marked.add(n);
-                dfs(b, res, marked, n, prefix);
+                dfs(b, words, marked, n, prefix);
             }
         }
-        String[] topKRes = topK(res, k);
-        for (int i = 0; i<k; i+=1){
-            String temp = topKRes[0];
-            topKRes[0] = topKRes[k-1-i];
-            topKRes[k-1-i] = temp;
-            sink(0, topKRes, k-1-i);
-        }
-        List<String> topK = new ArrayList<>(Arrays.asList(topKRes));
+        List<String> topK = topK(words, k);
         return topK;
     }
     
-    private static String[] topK(Set<String> res, int k) {
+    private static List<String> topK(MaxPQ<String> words, int k) {
+        List<String> topK = new ArrayList<>();
         int i = 0;
-        String[] topK = new String[k];
-        for (String s : res) {
-            if (i == 0) {
-                topK[0] = s;
-            } else if (i < k) {
-                topK[i] = s;
-                swim(i, topK);
-            } else {
-                if (!less(s, topK[0])) {
-                    topK[0] = s;
-                    sink(0, topK,k);
-                }
+        while (i < k && i < words.size()) {
+            String word = words.delMax();
+            if (!topK.contains(word)) {
+                topK.add(word);
+                i += 1;
             }
-            i += 1;
         }
         return topK;
     }
     
-    private static void sink(int i, String[] sList,int N) {
-        while (2 * i + 1 < N) {
-            int j = 2 * i + 1;
-            if (j < N - 1 && less(sList[j + 1], sList[j])) {
-                j += 1;
-            }
-            if (less(sList[i], sList[j])) {
-                break;
-            }
-            String temp = sList[j];
-            sList[j] = sList[i];
-            sList[i] = temp;
-            i = j;
-        }
-    }
-    
-    private static void swim(int i, String[] sList) {
-        int j = (i - 1) / 2;
-        if (i > 0 && !less(sList[j], sList[i])) {
-            String temp = sList[j];
-            sList[j] = sList[i];
-            sList[i] = temp;
-            swim(j, sList);
-        }
-    }
-    
-    private static boolean less(String s1, String s2) {
-        if (s1.length() == s2.length()) {
-            return s1.compareTo(s2) > 0;
-        }
-        return s1.length() < s2.length();
-    }
-    
-    private static void dfs(Board b, Set<String> res, List<Board.Node> marked, Board.Node n, String prefix) {
+    private static void dfs(Board b, MaxPQ<String> words, List<Board.Node> marked, Board.Node n, String prefix) {
         if (!trie.containPrefix(prefix)) {
             marked.remove(n);
             return;
         }
         if (prefix.length() > 2) {
-            if (trie.containWord(prefix) && !res.contains(prefix)) {
-                res.add(prefix);
+            if (trie.containWord(prefix)) {
+                words.insert(prefix);
             }
         }
         List<Board.Node> neighbors = b.getNeighbors(n);
@@ -125,7 +93,7 @@ public class Boggle {
                 tempMarked.add(n);
                 String prefixTemp = prefix;
                 prefixTemp += n.ch.toString();
-                dfs(b, res, tempMarked, n, prefixTemp);
+                dfs(b, words, tempMarked, n, prefixTemp);
             }
         }
     }
